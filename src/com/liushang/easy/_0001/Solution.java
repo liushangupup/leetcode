6481d95b7920cbc4f562c3a9201dab08a780cb90
liushangupup <976681935@qq.com>
2022-06-26 22:49:15 +0800

    暴力双循环解法

--- src/com/liushang/easy/_0001/Solution.java
+++ src/com/liushang/easy/_0001/Solution.java
@@ -1,9 +1,26 @@
 package com.liushang.easy._0001;
 
+import java.util.Arrays;
+
 //两数之和
 public class Solution {
 
+    //暴力法
+    public static int[] twoSum(int[] nums, int target) {
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+
     public static void main(String[] args) {
-        System.out.println("不要忘记，我不会是个笑话");
+        int[] nums = new int[]{1, 2, 3};
+        int target = 4;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
     }
 }
